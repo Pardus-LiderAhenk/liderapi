be8973264abf6738b18e54fe59cf887921d3fd2e
Hasan Kara <h.kara27@gmail.com>
2021-08-24 14:05:36 +0100

    added excel export service for detailed agent report

--- src/main/java/tr/org/lider/controllers/AgentInfoController.java
+++ src/main/java/tr/org/lider/controllers/AgentInfoController.java
@@ -1,5 +1,6 @@
 package tr.org.lider.controllers;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Optional;
@@ -7,7 +8,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tr.org.lider.entities.AgentImpl;
 import tr.org.lider.services.AgentService;
+import tr.org.lider.services.ExcelExportService;
 
 @Secured({"ROLE_ADMIN", "ROLE_AGENT_INFO" })
 @RestController
@@ -25,6 +30,9 @@ public class AgentInfoController {
 	@Autowired
 	private AgentService agentService;
 
+	@Autowired
+	private ExcelExportService excelService;
+	
 	@RequestMapping(value="/getInnerHtmlPage", method = {RequestMethod.POST })
 	public String getInnerHtmlPage(@RequestParam (value = "innerPage", required = true) String innerPage) {
 		return innerPage;
@@ -87,4 +95,40 @@ public class AgentInfoController {
 		}
 	}
 
+	@RequestMapping(method=RequestMethod.POST, value = "/export", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> export(
+			@RequestParam (value = "registrationStartDate") @DateTimeFormat(pattern="dd/MM/yyyy HH:mm:ss") Optional<Date> registrationStartDate,
+			@RequestParam (value = "registrationEndDate") @DateTimeFormat(pattern="dd/MM/yyyy HH:mm:ss") Optional<Date> registrationEndDate,
+			@RequestParam (value = "status") Optional<String> status,
+			@RequestParam (value = "dn") Optional<String> dn,
+			@RequestParam (value = "hostname") Optional<String> hostname,
+			@RequestParam (value = "macAddress") Optional<String> macAddress,
+			@RequestParam (value = "ipAddress") Optional<String> ipAddress,
+			@RequestParam (value = "brand") Optional<String> brand,
+			@RequestParam (value = "model") Optional<String> model,
+			@RequestParam (value = "processor") Optional<String> processor,
+			@RequestParam (value = "osVersion") Optional<String> osVersion,
+			@RequestParam (value = "agentVersion") Optional<String> agentVersion) {
+		Page<AgentImpl> listOfAgents = agentService.findAllAgents(
+				1, 
+				agentService.count().intValue(), 
+				registrationStartDate, 
+				registrationEndDate, 
+				status, 
+				dn,
+				hostname, 
+				macAddress, 
+				ipAddress, 
+				brand, 
+				model, 
+				processor, 
+				osVersion, 
+				agentVersion);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("fileName", "Istemci Raporu_" + new SimpleDateFormat("dd_MM_yyyy_HH:mm:ss.SSS").format(new Date()) + ".xlsx");
+		headers.setContentType(MediaType.parseMediaType("application/csv"));
+		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		return new ResponseEntity<byte[]>(excelService.generateAgentReport(listOfAgents.getContent()), headers,  HttpStatus.OK);
+	}
 }
---
+++ src/main/java/tr/org/lider/services/ExcelExportService.java
@@ -0,0 +1,299 @@
+package tr.org.lider.services;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import tr.org.lider.entities.AgentImpl;
+import tr.org.lider.entities.AgentPropertyImpl;
+
+@Service
+public class ExcelExportService {
+
+	Logger logger = LoggerFactory.getLogger(ExcelExportService.class);
+
+	public byte[] generateAgentReport(List<AgentImpl> agents) {
+		int rowCount = 0;
+		String exportFile = getFileWriteLocation() 
+				+ "İstemci Raporu_" 
+				+ new SimpleDateFormat("ddMMyyyyHH:mm:ss.SSS").format(new Date())
+				+ ".xlsx";
+		XSSFWorkbook wb = new XSSFWorkbook();
+
+		Font fontTextColourRed = wb.createFont();
+		fontTextColourRed.setColor(IndexedColors.RED.getIndex());
+
+		Font ftArial = wb.createFont();
+		ftArial.setFontName("Arial");
+
+		Font fontTextBold = wb.createFont();
+		fontTextBold.setBold(true);
+		fontTextBold.setFontName("Arial");
+		fontTextBold.setFontHeightInPoints((short) 10);
+
+		CellStyle csBoldAndBordered = wb.createCellStyle();
+		csBoldAndBordered.setFont(fontTextBold);
+		csBoldAndBordered.setBorderBottom(BorderStyle.THIN);
+		csBoldAndBordered.setBorderTop(BorderStyle.THIN);
+		csBoldAndBordered.setBorderLeft(BorderStyle.THIN);
+		csBoldAndBordered.setBorderRight(BorderStyle.THIN);
+		
+		CellStyle csBordered = wb.createCellStyle();
+		csBordered.setBorderBottom(BorderStyle.THIN);
+		csBordered.setBorderTop(BorderStyle.THIN);
+		csBordered.setBorderLeft(BorderStyle.THIN);
+		csBordered.setBorderRight(BorderStyle.THIN);
+
+		CellStyle csTextColourRed = wb.createCellStyle();
+		csTextColourRed.setFont(fontTextColourRed);
+
+		CellStyle csTextBold= wb.createCellStyle();
+		csTextBold.setFont(fontTextBold);
+
+		CellStyle csCenter = wb.createCellStyle();
+		csCenter.setAlignment(HorizontalAlignment.CENTER);
+		csCenter.setFont(ftArial);
+
+		XSSFSheet sheet = wb.createSheet("Detaylı İstemci Raporu");
+
+		//Add header
+		Row row = null; 
+		Cell cell = null;
+
+		int maxCountOfMacAddresses = 0;
+		int maxCountOfIPAddresses = 0;
+		
+		List<Integer> colWidthList = new ArrayList<Integer>();
+		List<String> headers = new ArrayList<String>();
+		
+		Collections.addAll(headers, "", "MAC", "Durumu");
+		Collections.addAll(colWidthList, 2500, 3500, 4500);
+		for (AgentImpl agent : agents) {
+			if(maxCountOfIPAddresses < agent.getIpAddresses().split(",").length) {
+				maxCountOfIPAddresses = agent.getIpAddresses().split(",").length;
+			}
+			if(maxCountOfMacAddresses < agent.getMacAddresses().split(",").length) {
+				maxCountOfMacAddresses = agent.getIpAddresses().split(",").length;
+			}
+		}
+
+		for (int i = 0; i < maxCountOfMacAddresses; i++) {
+			headers.add("MAC Adresi " + String.valueOf(i+1));
+			colWidthList.add(5000);
+		}
+
+		for (int i = 0; i < maxCountOfIPAddresses; i++) {
+			headers.add("IP Adresi " + String.valueOf(i+1));
+			colWidthList.add(5000);
+		}
+
+		Collections.addAll(headers, "Oluşturulma Tarihi", "İşletim Sistemi Versiyonu", "Ahenk Versiyonu",
+				"Marka", "Model", "İşlemci", "RAM(GB)", "Toplam Disk Alanı(GB)", "Kullanılan Disk Alanı(GB)", "Boş Disk Alanı(GB)");
+		Collections.addAll(colWidthList, 5500, 11500, 6000, 7000, 4000, 12000, 4000, 6000, 6000, 6000);
+		row = sheet.createRow(rowCount++);
+		for (int i = 0; i < headers.size(); i++) {
+			sheet.setColumnWidth(i, colWidthList.get(i));
+			cell = row.createCell(i);
+			cell.setCellValue(headers.get(i));
+			cell.setCellStyle(csBoldAndBordered);
+		}
+		int counter = 1;
+		for (AgentImpl agent : agents) {
+			int colCount = 0;
+			row = sheet.createRow(rowCount++);  
+			cell = row.createCell(colCount++);
+			cell.setCellValue(String.valueOf(counter++));
+			cell.setCellStyle(csBordered);
+			
+			String osVersion = "";
+			String agentVersion = "";
+			String processor = "";
+			String brand = "";
+			String model = "";
+			String memory = "";
+			String diskTotal = "";
+			String diskUsed = "";
+			int diskTotalCeil = 0;
+			Double diskUsedDouble = 0.0;
+			for (AgentPropertyImpl property: agent.getProperties()) {
+				if(property.getPropertyName().equals("os.version")) {
+					osVersion = property.getPropertyValue();
+				}
+				if(property.getPropertyName().equals("agentVersion")) {
+					agentVersion = property.getPropertyValue();
+				}
+				if(property.getPropertyName().equals("processor")) {
+					processor = property.getPropertyValue();
+				}
+				if(property.getPropertyName().equals("hardware.baseboard.manufacturer")) {
+					brand = property.getPropertyValue();
+				}
+				if(property.getPropertyName().equals("hardware.baseboard.productName")) {
+					model = property.getPropertyValue();
+				}
+				if(property.getPropertyName().equals("hardware.memory.total")) {
+					double value = Integer.parseInt(property.getPropertyValue())/1000.0;
+					memory = String.valueOf((int) Math.ceil(value));
+				}
+				if(property.getPropertyName().equals("hardware.disk.total")) {
+					double value = Integer.parseInt(property.getPropertyValue())/1000.0;
+					diskTotalCeil = (int)Math.ceil(value);
+					diskTotal = String.valueOf((int)Math.ceil(value));
+				}
+				if(property.getPropertyName().equals("hardware.disk.used")) {
+					double value = Integer.parseInt(property.getPropertyValue())/1000.0;
+					diskUsedDouble = value;
+					diskUsed = String.valueOf(String.format(Locale.forLanguageTag("tr-TR"), "%.2f", value));
+				}
+			}
+
+			cell = row.createCell(colCount++);
+			cell.setCellValue(agent.getHostname());
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			if(agent.getIsOnline()) {
+				cell.setCellValue("Çevrimiçi");
+				cell.setCellStyle(csBordered);
+			} else {
+				cell.setCellValue("Çevrimdışı");
+				cell.setCellStyle(csBordered);
+			}
+
+			for (int i = 0; i < maxCountOfMacAddresses; i++) {
+				try {
+					cell = row.createCell(colCount++);
+					cell.setCellValue(agent.getMacAddresses().split(",")[i].replace("'", "").trim());
+					cell.setCellStyle(csBordered);
+				} catch (Exception e) {
+					cell.setCellValue("");
+					cell.setCellStyle(csBordered);
+				}
+			}
+
+			for (int i = 0; i < maxCountOfIPAddresses; i++) {
+				try {
+					cell = row.createCell(colCount++);
+					cell.setCellValue(agent.getIpAddresses().split(",")[i].replace("'", "").trim());
+					cell.setCellStyle(csBordered);
+				} catch (Exception e) {
+					cell.setCellValue("");			
+					cell.setCellStyle(csBordered);
+				}
+			}
+
+			cell = row.createCell(colCount++);
+			cell.setCellValue(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(agent.getCreateDate()));
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(osVersion);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(agentVersion);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(brand);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(model);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(processor);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(memory);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(diskTotal);
+			cell.setCellStyle(csBordered);
+
+			cell = row.createCell(colCount++);
+			cell.setCellValue(diskUsed);
+			cell.setCellStyle(csBordered);
+			
+			cell = row.createCell(colCount++);
+			cell.setCellValue(String.valueOf(String.format(Locale.forLanguageTag("tr-TR"), "%.2f", (diskTotalCeil - diskUsedDouble))));
+			cell.setCellStyle(csBordered);
+			
+			colCount = 0;
+		}
+
+		try {
+			FileOutputStream outputStream = new FileOutputStream(exportFile);
+			wb.write(outputStream);
+			wb.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return fileToByteCode(exportFile);
+	}
+
+	private String getFileWriteLocation() {
+		if(System.getProperty("user.dir").equals("/")) {
+			return "/opt/tomcat/webapps/files/";
+		}
+		return "/tmp/";
+	}
+
+	private  byte[] fileToByteCode(String filePath) {
+		FileInputStream fi;
+		try {
+			fi = new FileInputStream(filePath);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			int c;
+			while ((c = fi.read()) != -1) {
+				baos.write(c);
+			}
+			fi.close();
+			byte[] fileContent = baos.toByteArray();
+			baos.close();
+			deleteLocalFile(filePath);
+			return fileContent;
+		} catch (FileNotFoundException e) {
+			logger.error("File not found for converting to byte code. Error: " + e.getMessage());
+			return null;
+		} catch (IOException e) {
+			logger.error("Error occured while converting file to byte code. Error: " + e.getMessage());
+			return null;
+		}
+	}
+
+	private void deleteLocalFile(String path) {
+		File file = new File(path);
+		if (!file.isDirectory() && file.exists()) {
+			file.delete();
+		}
+	}
+
+}
