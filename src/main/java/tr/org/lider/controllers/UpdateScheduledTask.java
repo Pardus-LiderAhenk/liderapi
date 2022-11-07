254ce6c088c40e7cdb15601bb6cce373c5ad9095
3bru <16ebruarslan@gmail.com>
2022-11-07 12:07:42 +0300

    scheduled task cont. edited

--- src/main/java/tr/org/lider/controllers/UpdateScheduledTask.java
+++ src/main/java/tr/org/lider/controllers/UpdateScheduledTask.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +41,7 @@ public class UpdateScheduledTask {
 			  @ApiResponse(responseCode = "200", description = "Update scheduled task"),
 			  @ApiResponse(responseCode = "400", description = "Scheduled task id not found!", 
 			    content = @Content(schema = @Schema(implementation = String.class))) })
-	@PutMapping(value = "/update")
+	@PostMapping(value = "/update")
 	public ResponseEntity<?> updateScheduledTask(@RequestParam (value = "id", required=false) Long id,
 			@RequestParam (value = "cronExpression", required=false) String cronExpression) {
 		
@@ -55,10 +57,10 @@ public class UpdateScheduledTask {
 			  @ApiResponse(responseCode = "200", description = ""),
 			  @ApiResponse(responseCode = "400", description = "Scheduled task id  not found", 
 			    content = @Content(schema = @Schema(implementation = String.class))) })
-	@PutMapping(value = "/cancel")
-	public ResponseEntity<?> cancelScheduledTask(@RequestParam (value = "id", required=false) Long id) {
+	@PutMapping(value = "/cancel/id/{id}")
+	public ResponseEntity<?> cancelScheduledTask(@PathVariable Long id) {
 		
-		logger.info("Request received. URL: '/api/lider/scheduled-task/update'");
+		logger.info("Request received. URL: '/api/lider/scheduled-task/cancel'");
 		ResponseEntity<?> response = scheduledTaskService.cancelScheduledTask(id);
 		return ResponseEntity
 				.status(HttpStatus.OK)
