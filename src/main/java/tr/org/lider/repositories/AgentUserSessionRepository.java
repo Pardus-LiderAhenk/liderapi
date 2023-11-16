c32801d23e3846faf38ba7d4f510b7723a0090a9
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2023-11-16 16:51:25 +0300

    user session history added with paging

--- src/main/java/tr/org/lider/repositories/AgentUserSessionRepository.java
+++ src/main/java/tr/org/lider/repositories/AgentUserSessionRepository.java
@@ -2,12 +2,15 @@ package tr.org.lider.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import tr.org.lider.entities.UserSessionImpl;
 
 
 
 public interface AgentUserSessionRepository extends BaseJpaRepository<UserSessionImpl, Long>{
 
-	List<UserSessionImpl> findByUsername(String username);
+	Page<UserSessionImpl> findByUsername(String username, Pageable pageable);
 }
 
